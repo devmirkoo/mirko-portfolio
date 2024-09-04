0112e64150d7a6fc9acefef7a517d054e6aae961
mirkou <fiorinimirko2006@gmail.com>
2024-09-04 16:59:14 +0200

    refactor: Update basePath in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   output: 'export',
-  basePath: isProd ? '/portfolio' : '',
+  basePath: isProd ? '/mirko-portfolio' : '',
   reactStrictMode: true,
 
   images: {
