68de72227cd050c7bcaee0866e8aab5c182fdb5e
mirkou <fiorinimirko2006@gmail.com>
2024-09-25 14:32:34 +0200

    Refactor: Update next.config.mjs to include i18n settings

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,25 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
+
   output: 'export',
   basePath: isProd ? '/' : '',
   reactStrictMode: true,
 
   images: {
     unoptimized: true
+  },
+
+  i18n: {
+    locales: ['en-US', 'it-IT', 'fr', 'nl-NL', 'nl-BE'],
+    defaultLocale: 'en-US',
+
+    domains: [
+      {
+        domain: 'mirko.live',
+        defaultLocale: 'en-US',
+      },
+    ],
   },
 };
 
