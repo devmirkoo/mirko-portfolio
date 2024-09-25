7649249b84f028fa908f6a363f9544c9ac330839
mirkou <fiorinimirko2006@gmail.com>
2024-09-25 14:36:38 +0200

    Refactor: Update next.config.mjs to remove i18n settings and add reactStrictMode and unoptimized image settings

--- next.config.mjs
+++ next.config.mjs
@@ -3,25 +3,12 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-
   output: 'export',
   basePath: isProd ? '/' : '',
   reactStrictMode: true,
 
   images: {
     unoptimized: true
-  },
-
-  i18n: {
-    locales: ['en-US', 'it-IT', 'fr', 'nl-NL', 'nl-BE'],
-    defaultLocale: 'en-US',
-
-    domains: [
-      {
-        domain: 'mirko.live',
-        defaultLocale: 'en-US',
-      },
-    ],
   },
 };
 
